abfd1913a69f1c2af4dc0910124d7514d584fa95
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-03 21:47:55 +0530

    update: h1 for signin.js.

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -34,7 +34,7 @@ function SignIn() {
         userName = data._tokenResponse.screenName;
         userImageURL = data._tokenResponse.photoUrl || null;
       }
-      
+
       document.title = `${userName} - ${authName} Auth`;
       setAuthProvider(authName);
 
@@ -72,7 +72,7 @@ function SignIn() {
   }, [userFavicon]);
 
   return (
-    <div className="flex items-center justify-center h-screen bg-[#04162ef8] space-x-4">
+    <div className="flex justify-center h-screen bg-[#04162ef8]">
       {userData ? (
         <Welcome
           userName={userData.userName}
@@ -80,27 +80,37 @@ function SignIn() {
           authProvider={authProvider}
         />
       ) : (
-        <>
-          <span className="w-16 h-16 bg-white rounded-full select-none">
-            <button
-              className="bg-opacity-0 cursor-pointer"
-              onClick={() => authenticateUser(googleProvider, "Google")}
-              disabled={isLoading}
-            >
-              <img src="./gg.png" alt="Big G" className="w-16 h-16" />
-            </button>
-          </span>
+        <div className="flex flex-col items-center pt-40 sm:pt-48">
+          <h1 className="text-white mb-10 text-2xl sm:text-3xl md:text-4xl text-center"
+          style={{ fontFamily: "Lilita One, cursive" }}>
+            Whats it gonna be, Google or Github?
+          </h1>
+          <div className="flex space-x-4">
+            <span className="w-16 h-16 bg-white rounded-full select-none">
+              <button
+                className="bg-opacity-0 cursor-pointer"
+                onClick={() => authenticateUser(googleProvider, "Google")}
+                disabled={isLoading}
+              >
+                <img src="./gg.png" alt="Big G" className="w-16 h-16" />
+              </button>
+            </span>
 
-          <span className="w-16 h-16 bg-white rounded-full select-none flex align-center justify-center">
-            <button
-              className="bg-opacity-0 cursor-pointer"
-              onClick={() => authenticateUser(githubProvider, "GitHub")}
-              disabled={isLoading}
-            >
-              <img src="./github.svg" alt="GitHub Icon" className="w-12 h-12" />
-            </button>
-          </span>
-        </>
+            <span className="w-16 h-16 bg-white rounded-full select-none flex align-center justify-center">
+              <button
+                className="bg-opacity-0 cursor-pointer"
+                onClick={() => authenticateUser(githubProvider, "GitHub")}
+                disabled={isLoading}
+              >
+                <img
+                  src="./github.svg"
+                  alt="GitHub Icon"
+                  className="w-12 h-12"
+                />
+              </button>
+            </span>
+          </div>
+        </div>
       )}
     </div>
   );
