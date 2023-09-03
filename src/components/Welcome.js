755d990ae808c5242afa4eb036315a4486daeb0f
nithinK-142 <nithin.sagar.359@gmail.com>
2023-09-03 20:38:19 +0530

    update: welcome.js removed extra div, h1 text-center.

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -7,19 +7,33 @@ function Welcome({ userName, userImageURL, authProvider }) {
   };
 
   return (
-    <div className="flex flex-col items-center justify-start h-screen">
+    <div className="flex flex-col items-center h-screen pt-20">
+        {userImageURL && (
+          <img
+            src={userImageURL}
+            alt="UserImage"
+            className="w-36 h-36 mb-10 rounded-full object-cover"
+          />
+        )}
 
-      <div className="flex flex-col items-center justify-center pt-20">
+        <h1
+          className="text-white mb-10 text-2xl sm:text-3xl md:text-4xl text-center"
+          style={{ fontFamily: "Lilita One, cursive" }}
+        >
+          Welcome to the Code Side, {userName}!
+        </h1>
 
-        {userImageURL && ( <img src={userImageURL} alt="UserImage" className="w-36 h-36 mb-10 rounded-full object-cover"/>)}
-          
-        <h1 className="text-white mb-10 text-2xl sm:text-3xl md:text-4xl" style={{ fontFamily: 'Lilita One, cursive'}}>Welcome to the Code Side, {userName}!</h1>
+        <h2 className="text-[#B2D7E8] mb-10 text-sm sm:text-2xl md:text-3xl font-semibold">
+          Logged in with {authProvider}
+        </h2>
 
-        <h2 className="text-[#B2D7E8] mb-10 text-sm sm:text-2xl md:text-3xl font-semibold">Logged in with {authProvider}</h2>
-          
-        <button className="px-6 py-2 bg-red-500 text-white border-none rounded-md" onClick={logout}>Logout</button>
-      </div>
-  </div>
+        <button
+          className="px-6 py-2 bg-red-500 text-white border-none rounded-md"
+          onClick={logout}
+        >
+          Logout
+        </button>
+    </div>
   );
 }
 
